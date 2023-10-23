1f938c007d2160f9b50d953146a9fceb3ff7adbc
Myles Penner <mj.penner@live.ca>
2023-10-22 21:26:12 -0700

    Updating Registration and Login forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,61 @@
 import React, { useState, useEffect } from "react";
-import { Route, Routes, Link, Navigate } from "react-router-dom";
+import { Route, Routes, Link, Navigate, useNavigate } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import VaultDashboard from "./pages/VaultDashboard";
 import Login from "./pages/Login";
 import Tools from "./pages/Tools";
 import Register from "./pages/Register";
 
-function PrivateRoute({ children, ...rest }) {
-  const isAuthenticated = true; // Just for now
+function PrivateRoute({ children, isAuthenticated }) {
+  // const isAuthenticated = true; // Just for now
 
-  if (isAuthenticated) {
-    return children;
-  }
+  const navigate = useNavigate();
 
-  return <Navigate to="/login" replace />
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("/login");
+    }
+  }, [isAuthenticated, navigate]);
+
+  return isAuthenticated ? children : null;
 }
 
 function App() {
   const [items, setItems] = useState([]);
-  const [isAuthenticated, setIsAuthenticated] = useState(true)  // For now
+  const [isAuthenticated, setIsAuthenticated] = useState(true); // For now
 
   const handleLogout = () => {
-    setIsAuthenticated(false)
-  }
+    setIsAuthenticated(false);
+  };
 
   return (
     <div className="App">
-      {isAuthenticated && <Navigation onLogout={handleLogout}/>}
+      {isAuthenticated && <Navigation onLogout={handleLogout} />}
       <header>
         <h1>VaultMaster</h1>
       </header>
       <main>
         <Routes>
           <Route path="/" element={<Navigate to="/vault" replace />} />
-          {/* Replace - new location replaces current entry to back button nav still works */}
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
 
-          {/* Protected Routes */}
-          <PrivateRoute path="/vault" element={<VaultDashboard />} />
-          <PrivateRoute path="/tools" element={<Tools />} />
+          <Route
+            path="/vault"
+            element={
+              <PrivateRoute isAuthenticated={isAuthenticated}>
+                <VaultDashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/tools"
+            element={
+              <PrivateRoute isAuthenticated={isAuthenticated}>
+                <Tools />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </main>
     </div>
