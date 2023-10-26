a67f52f1b700c93d810f6552aa28a5f1002b5dac
MylesJP <96148570+MylesJP@users.noreply.github.com>
2023-10-26 11:12:38 -0700

    Updated Profile

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Profile() {
   return (
-    <div>Profile</div>
+    <div className="profile">
+      <h1>User Profile</h1>
+      <h2>Display information about the user here and ability to delete account</h2>
+    </div>
   )
 }
