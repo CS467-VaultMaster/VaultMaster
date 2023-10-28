4746f15d9579ff9f6344e6e441de4d3b19de3d7a
MylesJP <96148570+MylesJP@users.noreply.github.com>
2023-10-27 19:10:55 -0700

    Updated VaultDashboard to display info

--- frontend/src/pages/VaultDashboard.js
+++ frontend/src/pages/VaultDashboard.js
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navigation from "../components/Navigation";
+import axios from "axios";
 
 function VaultDashboard() {
+  const [credentials, setCredentials] = useState([]);
+
+  useEffect(() => {
+    const fetchCredentials = async () => {
+      try {
+        const token = sessionStorage.getItem("authToken");
+
+        if (!token) {
+          console.error("No authentication token found.");
+          return;
+        }
+
+        const response = await axios.get(
+          `${process.env.REACT_APP_FASTAPI_URL}/vaultmaster/vault`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        setCredentials(response.data);
+      } catch (error) {
+        console.error("Error fetching credentials:", error);
+      }
+    };
+    fetchCredentials();
+  }, []);
+
   return (
     <div className="vault-dashboard">
-      {/* <Navigation /> */}
-      <h2>Put vault dashboard information here</h2>
+      <h2>Dashboard</h2>
+      <ul>
+        {credentials.map((credential) => (
+          <li key={credential.id}>
+            <strong>Nickname:</strong> {credential.nickname}
+            <br />
+            <strong>URL:</strong> {credential.url}
+            <br />
+            <strong>Password:</strong> {credential.password}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
