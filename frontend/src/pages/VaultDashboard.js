1ed09726611ef20bb73010651e08a67ec9a998cc
MylesJP <96148570+MylesJP@users.noreply.github.com>
2023-10-31 12:21:08 -0700

    Vault access verification

--- frontend/src/pages/VaultDashboard.js
+++ frontend/src/pages/VaultDashboard.js
@@ -4,59 +4,104 @@ import axios from "axios";
 
 function VaultDashboard() {
   const [credentials, setCredentials] = useState([]);
+  const [password, setPassword] = useState("");
+  const [isPwdVerified, setIsPwdVerified] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
-  useEffect(() => {
-    const fetchCredentials = async () => {
-      try {
-        const token = sessionStorage.getItem("authToken");
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-        if (!token) {
-          console.error("No authentication token found.");
-          return;
+  const handlePasswordSubmit = async () => {
+    try {
+      setErrorMessage("");
+      console.log(`Password: ${password}`)
+      // Getting a 401 for some reason even with the correct password
+      const response = await axios.put(
+        `${process.env.REACT_APP_FASTAPI_URL}/vaultmaster/vault/open`,
+        {
+          password: password,
         }
-
-        const response = await axios.get(
-          `${process.env.REACT_APP_FASTAPI_URL}/vaultmaster/vault`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
-        setCredentials(response.data);
-      } catch (error) {
-        console.error("Error fetching credentials:", error);
+      );
+      if (response.status === 200) {
+        setIsPwdVerified(true);
+        setSuccessMessage("Verification successful. Unlocking vault...");
+        setTimeout(() => {
+          fetchCredentials();
+          setSuccessMessage("");
+        }, 2000);
+      } else {
+        setErrorMessage("Incorrect password.");
       }
-    };
-    fetchCredentials();
-  }, []);
+    } catch (error) {
+      console.error("An error occured while verifying the password: ", error);
+      setErrorMessage("An error occured while verifying the password.");
+    }
+  };
+
+  const fetchCredentials = async () => {
+    try {
+      const token = sessionStorage.getItem("authToken");
+      if (!token) {
+        console.error("No authentication token found.");
+        return;
+      }
+
+      const response = await axios.get(
+        `${process.env.REACT_APP_FASTAPI_URL}/vaultmaster/vault`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setCredentials(response.data);
+    } catch (error) {
+      console.error("Error fetching credentials:", error);
+    }
+  };
 
   return (
     <div className="vault-dashboard">
-      <h2>Dashboard</h2>
-      {credentials.length > 0 ? (
-        <table>
-          <thead>
-            <tr>
-              <th>Nickname</th>
-              <th>URL</th>
-              <th>Password</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>
-            {credentials.map(credential => (
-              <tr key={credential.id}>
-                <td>{credential.nickname}</td>
-                <td>{credential.url}</td>
-                <td>{credential.password}</td>
-                <td>{credential.category}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <h2>Vault Dashboard</h2>
+      {!isPwdVerified ? (
+        <div>
+          <label>
+            Password:
+            <input type="password" value={password} onChange={handlePasswordChange} />
+          </label>
+          <button onClick={handlePasswordSubmit}>Verify</button>
+          {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+        </div>
       ) : (
-        <p>No credentials found.</p>
+        <div>
+          {successMessage && <p>{successMessage}</p>}
+          {credentials.length > 0 ? (
+            <table>
+              <thead>
+                <tr>
+                  <th>Nickname</th>
+                  <th>URL</th>
+                  <th>Password</th>
+                  <th>Category</th>
+                </tr>
+              </thead>
+              <tbody>
+                {credentials.map((credential) => (
+                  <tr key={credential.id}>
+                    <td>{credential.nickname}</td>
+                    <td>{credential.url}</td>
+                    <td>{credential.password}</td>
+                    <td>{credential.category}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p>No credentials found.</p>
+          )}
+        </div>
       )}
     </div>
   );
