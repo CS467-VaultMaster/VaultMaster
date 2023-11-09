ec40ff0d6f9833f017a3100eba380992e6effb00
will_lambeth <wflambeth@gmail.com>
2023-11-09 13:59:53 -0800

    Add sleep period to backend startup

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Give the database a few seconds to start
+sleep 5
+
+# Apply migrations
 if [ -z "$PG_AWS_REGION" ]; then
     alembic stamp head
     alembic revision --autogenerate -m "Migration message"
