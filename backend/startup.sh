c56121d9391c20a75b0a4fa9de25d6daefa75708
will_lambeth <wflambeth@gmail.com>
2023-11-16 12:26:47 -0800

    Move db wait inside local conditional

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Give the database a few seconds to start
-sleep 5
-
-# Apply migrations
+# i.e. if running locally
 if [ -z "$PG_AWS_REGION" ]; then
+    # Give the database a few seconds to start
+    sleep 5
+    
+    # Apply Alembic migrations
     alembic stamp head
     alembic revision --autogenerate -m "Migration message"
     alembic upgrade head
